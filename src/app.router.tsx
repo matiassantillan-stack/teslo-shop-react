53170750d125c2be7cac3151ada578b02caa26ba
matias.santillan <matias.santillan@proveedor.ues21.edu.ar>
2026-04-20 12:57:14 -0300

    remove createBrowserRouter

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,5 +1,8 @@
 import { lazy } from "react";
-import { createBrowserRouter, createHashRouter, Navigate } from "react-router";
+import {
+  /*createBrowserRouter,*/ createHashRouter,
+  Navigate,
+} from "react-router";
 
 import { ShopLayout } from "./shop/layouts/ShopLayout";
 import { HomePage } from "./shop/pages/home/HomePage";
